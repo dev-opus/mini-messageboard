af2b6affa5e0ddbf667e5ee09f7ee2dcae2a2821
Victor Orlunda <vicorlunda@gmail.com>
2021-03-05 23:59:36 +0100

    add static file middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const { urlencoded } = require('express');
 const express = require('express');
 const app = express();
+const path = require('path');
+
 const exphbs = require('express-handlebars');
 const port = process.env.PORT || 8080;
 
@@ -8,9 +10,15 @@ const port = process.env.PORT || 8080;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// static files middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
 // set-up template-engine
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: 'hbs' }));
 app.set('view engine', 'hbs');
+
+// router level middleware
+app.use('/', require('./routes/index'));
 
 // fire up the server!
 app.listen(port, () => {
