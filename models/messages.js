2196ee7e8755c29374a6eada719387e47a040ec0
Victor Orlunda <vicorlunda@gmail.com>
2021-03-06 06:27:51 +0100

    add data to messages module

---
+++ models/messages.js
@@ -0,0 +1,14 @@
+const messages = [
+  {
+    text: 'Cash is no distraction as long as Passion is the glue',
+    user: 'crunchy',
+    added: new Date(),
+  },
+  {
+    text: 'To live, is to suffer',
+    user: 'hiatus_',
+    added: new Date(),
+  },
+];
+
+module.exports = messages;
