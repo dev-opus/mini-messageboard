2a7279a4d60253ad981f5c038f0f4e841f6c99f5
Victor Orlunda <vicorlunda@gmail.com>
2021-03-28 20:37:08 +0100

    implement login routes

---
+++ routes/login.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+
+router.get('/', (req, res) => {
+  res.render('login');
+});
+
+router.post(
+  '/',
+  passport.authenticate('local', {
+    successRedirect: '/index',
+    failureRedirect: '/login',
+  })
+);
+
+module.exports = router;
