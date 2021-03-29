1ddfd41834b97a2ebdceea6e5d1c9e091ed8a8dd
Victor Orlunda <vicorlunda@gmail.com>
2021-03-29 06:20:26 +0100

    update sign up route to restrict creating users without a password field

--- routes/register.js
+++ routes/register.js
@@ -9,12 +9,19 @@ router.get('/', (req, res) => {
 
 router.post('/', async (req, res) => {
   try {
+    if (
+      req.body.password === undefined ||
+      req.body.password === '' ||
+      /^\s+$/.test(req.body.password)
+    ) {
+      res.render('register', { password: 'Password must not be empty' });
+    }
     const saltHash = genPassword(req.body.password);
     const { salt, hash } = saltHash;
     const testUser = await User.findOne({ username: req.body.username }).lean();
 
     if (testUser) {
-      return res.render('register', {already: 'Username taken!'})
+      return res.render('register', { already: 'Username taken!' });
     }
 
     const newUser = new User({
